bdb941c79b99466952c597067ae1dc815a1b35b9
Sumit Bisht <sumit7beazt@gmail.com>
2023-04-26 22:19:35 +0530

    first commit

---
+++ src/features/icecream/icecreamSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    numOfIcecreams: 20
+}
+
+const icecreamSlice = createSlice({
+    name: 'icecream',
+    initialState,
+    reducers: {
+        ordered: (state) => {
+            state.numOfIcecreams--;
+        },
+        restocked: (state, action) => {
+            state.numOfIcecreams += action.payload
+        }
+    }
+})
+
+export default icecreamSlice.reducer
+export const { ordered, restocked } = icecreamSlice.actions
